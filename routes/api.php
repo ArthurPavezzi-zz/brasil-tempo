c09190072e2d9c07a9ba48b98db1eeb7fc94bb6e
Arthur Pavezzi <arthur.henrique.pavezzi@gmail.com>
2020-11-12 01:34:36 -0300

    Added API routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +17,29 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/weather', function () {
+    $geocode = \request('geocode');
+    $response = Http::get("https://apiprevmet3.inmet.gov.br/previsao/$geocode");
+    return $response->json();
+});
+
+Route::get('/state-code', function () {
+    $stateName = \request('name');
+    $response = Http::get("https://servicodados.ibge.gov.br/api/v1/localidades/estados/$stateName");
+    return $response->json();
+});
+
+Route::get('/city-code', function () {
+
+    $stateCode = \request('stateCode');
+    $cityName = \request('cityName');
+    $response = Http::get("https://servicodados.ibge.gov.br/api/v1/localidades/estados/$stateCode/municipios");
+
+    foreach ($response->json() as $city) {
+        if ($city['nome'] === $cityName) {
+            return json_encode($city);
+        }
+    }
 });
